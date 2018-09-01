76013788b947a20c02332f376f12d4b35222fd53
IliyaZinoviev <mrmar333@yandex.ru>
2018-09-01 16:06:48 +0300

    remove websocketserver.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require("http");
 var fs = require("fs");
+var WebSocketServer = new require('ws');
  
-http.createServer(function(request, response){
+var server = http.createServer(function(request, response){
     console.log(`Запрошенный адрес: ${request.url}`);
     if(request.url.startsWith("/public/")){
         var filePath = request.url.substr(1);
@@ -16,4 +17,33 @@ http.createServer(function(request, response){
             return;
         })
     }
-}).listen(3000);
+}).listen(3000, () => {console.log('server work on 3000')});
+
+// подключенные клиенты
+var clients = {};
+
+// WebSocket-сервер на порту 8081
+var webSocketServer = new WebSocketServer.Server({
+    server: server
+});
+
+webSocketServer.on('connection', function(ws) {
+
+  var id = Math.random();
+  clients[id] = ws;
+  console.log("новое соединение " + id);
+
+  ws.on('message', function(message) {
+    console.log('получено сообщение ' + message);
+
+    for (var key in clients) {
+      clients[key].send(message);
+    }
+  });
+
+  ws.on('close', function() {
+    console.log('соединение закрыто ' + id);
+    delete clients[id];
+  });
+
+});
